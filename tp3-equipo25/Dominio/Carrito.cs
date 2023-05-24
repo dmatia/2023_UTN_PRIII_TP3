369968b99086965994c12cfb8cb6378d0610379d
dmatia <diegovillalba.96@gmail.com>
2023-05-24 20:19:01 -0300

    Agrego Clase Carrito a Dominio

---
+++ tp3-equipo25/Dominio/Carrito.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Carrito
+    {
+        public Articulo Articulo { get; set; } 
+        public int Cantidad { get; set; }
+    }
+}
