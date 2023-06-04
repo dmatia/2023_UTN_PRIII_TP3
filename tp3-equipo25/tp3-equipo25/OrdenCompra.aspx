f365e36d81aaaef6a0c0ab539cca0a80e78a7861
dmatia <diegovillalba.96@gmail.com>
2023-06-04 18:17:09 -0300

    Elimino scriptmanager de orden de compra

--- tp3-equipo25/tp3-equipo25/OrdenCompra.aspx
+++ tp3-equipo25/tp3-equipo25/OrdenCompra.aspx
@@ -3,7 +3,6 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Main" runat="server">
-    <asp:ScriptManager runat="server" ID="SM_Detalle"></asp:ScriptManager>
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
--- tp3-equipo25/tp3-equipo25/OrdenCompra.aspx.designer.cs
+++ tp3-equipo25/tp3-equipo25/OrdenCompra.aspx.designer.cs
@@ -11,61 +11,52 @@ namespace tp3_equipo25
 {
 
 
-	public partial class OrdenCompra
-	{
+    public partial class OrdenCompra
+    {
 
-		/// <summary>
-		/// SM_Detalle control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.ScriptManager SM_Detalle;
+        /// <summary>
+        /// lbModoEnvio control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbModoEnvio;
 
-		/// <summary>
-		/// lbModoEnvio control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.Label lbModoEnvio;
+        /// <summary>
+        /// lbModoPago control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbModoPago;
 
-		/// <summary>
-		/// lbModoPago control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.Label lbModoPago;
+        /// <summary>
+        /// repeaterOrdenCompra control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater repeaterOrdenCompra;
 
-		/// <summary>
-		/// repeaterOrdenCompra control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.Repeater repeaterOrdenCompra;
+        /// <summary>
+        /// modoPago control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList modoPago;
 
-		/// <summary>
-		/// modoPago control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.DropDownList modoPago;
-
-		/// <summary>
-		/// modoEnvio control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.DropDownList modoEnvio;
-	}
+        /// <summary>
+        /// modoEnvio control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList modoEnvio;
+    }
 }
